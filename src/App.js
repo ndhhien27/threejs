1c1d3d723d5a581d26c1658a79b526643bc42e48
Hien Nguyen Duc <hiennd@tech.est-rouge.com>
2022-10-30 22:11:41 +0700

    Transform

--- src/App.js
+++ src/App.js
@@ -14,11 +14,17 @@ function App() {
   const redBox = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshBasicMaterial({ color: "red" });
   const mesh = new THREE.Mesh(redBox, material);
+  mesh.position.set(0.7, -0.6, 1);
 
   scene.add(mesh);
 
+  const axesHelper = new THREE.AxesHelper(1);
+  scene.add(axesHelper);
+
   const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
-  camera.position.z = 5;
+  camera.position.z = 3;
+  camera.position.x = 1;
+  camera.position.y = 1;
   scene.add(camera);
 
   useEffect(() => {
